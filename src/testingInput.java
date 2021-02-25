6994feb0b954347b0d1533404ec10f743c0343e9
Aozin <59066161+Aozin@users.noreply.github.com>
2021-02-25 20:42:14 +0000

    f

--- src/testingInput.java
+++ src/testingInput.java
@@ -6,10 +6,30 @@ import java.util.Arrays;
 
 public class testingInput {
     public static void main(String[] args) {
-
+        /**
         Parser test = new Parser("b.txt");
         for (Intersection x : test.intersections) {
             System.out.println(x);
         }
+         **/
+
+        Parser test = new Parser("b.txt");
+        String output;
+        StringBuilder str = new StringBuilder();
+
+        str.append(test.numIntersections);
+        str.append("\n");
+        for (Intersection x : test.intersections) {
+            str.append(x.id);
+            str.append("\n");
+            str.append(x.getInStreets().size());
+            str.append("\n");
+            for (Street i : x.getInStreets()) {
+                str.append(i.getName() + " 1");
+                str.append("\n");
+            }
+        }
+        System.out.println(str.toString());
+
     }
 }
