2e0e1178cb1791e7bc01ec48030e9b26d6ec6417
sergiorivera50 <sriveralopez50@gmail.com>
2021-02-25 19:28:33 +0100

    Added queue to TrafficLight

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,7 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TrafficLight {
     private Intersection intersection;
     private TrafficState state = TrafficState.RED;
     private Street street;
+    private Queue<Car> waitingCars = new LinkedList<>();
 
     public TrafficLight(Street street, Intersection intersection) {
         this.street = street;
@@ -22,6 +26,16 @@ public class TrafficLight {
         } else {
             state = TrafficState.GREEN;
         }
+    }
+
+    public void carArrives(Car car) {
+        waitingCars.add(car);
+    }
+
+    public Car carLeaves() {
+        Car leavingCar = waitingCars.peek();
+        waitingCars.remove();
+        return leavingCar;
     }
 
     public String toString() {
